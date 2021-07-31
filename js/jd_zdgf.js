d947027bd4fdffe350ed3fc53b4a398167dbfc83
iroyway <51533547+iroyway@users.noreply.github.com>
2021-07-31 17:36:14 +0800

    Update jd_zdgf.js

--- js/jd_zdgf.js
+++ js/jd_zdgf.js
@@ -10,11 +10,11 @@ var queryStr = jUrl.split("?")[1]
 
 var actUrl = jUrl.split(`/`);
 if (actUrl[2]) {
-    if (actUrl && actUrl.length == 32) {
-        actUrl = (actUrl.slice(0, 3)).join("/");
+    if (actUrl && actUrl.length ！== 32) {
+        $.done();        
     }
     else {
-        $.done();
+        actUrl = (actUrl.slice(0, 3)).join("/");
     }
 } else {
     actUrl = "";
