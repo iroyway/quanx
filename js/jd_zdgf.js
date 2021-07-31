9bef958123654dadf2f5809bde82d475d6e5e13a
iroyway <51533547+iroyway@users.noreply.github.com>
2021-07-31 18:03:28 +0800

    Update jd_zdgf.js

--- js/jd_zdgf.js
+++ js/jd_zdgf.js
@@ -10,7 +10,7 @@ var queryStr = jUrl.split("?")[1]
 
 var actUrl = jUrl.split(`/`);
 if (actUrl[2]) {
-    if (actUrl && actUrl.length ！== 32) {
+    if (actUrl && actUrl.length < 32) {
         $.done();        
     }
     else {
